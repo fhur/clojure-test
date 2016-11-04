4be2caf53f979b4677cccbf53bb0d27a179bc083
Fernando Hurtado <fernandohur@gmail.com>
2016-11-04 08:34:54 +0100

    second commit

---
+++ src/test_macros/learn.clj
@@ -0,0 +1,63 @@
+(ns test-macros.learn)
+
+;; boolean
+=
+or
+and
+not
+
+;; number related
+>
+<
+>=
+<=
++
+*
+-
+/
+inc
+dec
+
+;; IO
+println
+
+;; functions
+fn
+defn
+
+;; Branching
+if
+cond
+do
+
+;; Collections
+conj
+empty?
+rest
+cons
+first
+second
+contains?
+into
+count
+
+;; Maps
+assoc
+dissoc
+keys
+get-in
+merge
+update-in
+
+;;
+map
+filter
+reduce
+
+;; keyword
+name
+instance?
+
+;; Macros
+->>
+loop recur
